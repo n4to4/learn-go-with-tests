e1549a47b0be9c4d885b88607afcaafea73ef0ad
n4to4 <n4to4k@gmail.com>
2021-06-27 14:05:46 +0900

    spanish

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix string = "Hello, "
+const (
+	spanish                   = "Spanish"
+	englishHelloPrefix string = "Hello, "
+	spanishHelloPrefix string = "Hola, "
+)
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
