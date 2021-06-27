f72d7a7207f599e0f7531ad90395d3be87c3519c
n4to4 <n4to4k@gmail.com>
2021-06-27 23:21:59 +0900

    empty slices~

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,19 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers[1:]))
+	}
+	return sums
+}
