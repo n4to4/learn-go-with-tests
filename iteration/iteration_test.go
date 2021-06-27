6abf1802bfeb98a3c1e26213ca83c640c238ce5a
n4to4 <n4to4k@gmail.com>
2021-06-27 22:34:50 +0900

    more tests

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,18 +1,39 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
-
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	assertRepeatN := func(t *testing.T, repeated, expected string) {
+		t.Helper()
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
 	}
+
+	t.Run("repeat 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertRepeatN(t, repeated, expected)
+	})
+
+	t.Run("repeat 11 times", func(t *testing.T) {
+		repeated := Repeat("b", 11)
+		expected := "bbbbbbbbbbb"
+		assertRepeatN(t, repeated, expected)
+	})
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 4)
+	fmt.Println(repeated)
+	// Output: aaaa
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
