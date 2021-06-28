e4d1b6d5378a4923bf81d161ea1688d4fd5a7f8a
n4to4 <n4to4k@gmail.com>
2021-06-28 18:34:47 +0900

    refactor tests

--- structs/shapes.go
+++ structs/shapes.go
@@ -31,3 +31,16 @@ func (c Circle) Perimeter() float64 {
 func (c Circle) Area() float64 {
 	return c.Radius * c.Radius * math.Pi
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Perimeter() float64 {
+	return 0
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
