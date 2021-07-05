69c865e01119bb0945dbd88985fa3b9923238304
n4to4 <n4to4k@gmail.com>
2021-07-05 17:14:12 +0900

    refactor

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,4 +1,4 @@
-package reflect
+package reflection
 
 import "testing"
 
@@ -16,5 +16,9 @@ func TestWalk(t *testing.T) {
 
 	if len(got) != 1 {
 		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
 	}
 }
