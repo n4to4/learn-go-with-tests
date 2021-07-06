24337cd82f996b54de0862110cc56d38bf03d695
n4to4 <n4to4k@gmail.com>
2021-07-06 20:29:00 +0900

    concurrent

--- sync/sync.go
+++ sync/sync.go
@@ -1,8 +1,15 @@
 package sync_
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
-func (c *Counter) Inc()       { c.value++ }
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
 func (c *Counter) Value() int { return c.value }
