949a4c1a643646d5832c17cac3b413a30bcf3b68
n4to4 <n4to4k@gmail.com>
2021-07-07 21:01:10 +0900

    pass

--- context/context_test.go
+++ context/context_test.go
@@ -2,6 +2,7 @@ package context
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -25,22 +26,24 @@ func TestServer(t *testing.T) {
 		}
 	})
 
-	//t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-	//	store := &SpyStore{response: data, t: t}
-	//	svr := Server(store)
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
 
-	//	request := httptest.NewRequest(http.MethodGet, "/", nil)
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
 
-	//	cancellingCtx, cancel := context.WithCancel(request.Context())
-	//	time.AfterFunc(5*time.Millisecond, cancel)
-	//	request = request.WithContext(cancellingCtx)
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
 
-	//	response := httptest.NewRecorder()
+		response := &SpyResponseWriter{}
 
-	//	svr.ServeHTTP(response, request)
+		svr.ServeHTTP(response, request)
 
-	//	store.assertWasCancelled()
-	//})
+		if response.written {
+			t.Error("a response should not have been written")
+		}
+	})
 }
 
 type SpyStore struct {
@@ -72,4 +75,22 @@ func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
 	case res := <-data:
 		return res, nil
 	}
+}
+
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
 }
