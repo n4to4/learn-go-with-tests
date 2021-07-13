e89e5a1a9f9dced41b88fdfb37d9bc46c432c0ba
n4to4 <n4to4k@gmail.com>
2021-07-13 19:15:03 +0900

    refactor

--- reading-files/post.go
+++ reading-files/post.go
@@ -28,26 +28,19 @@ func newPost(postFile io.Reader) (Post, error) {
 		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
 
-	title := readMetaLine(titleSeparator)
-	description := readMetaLine(descriptionSeparator)
-	tags := strings.Split(readMetaLine(tagsSeparator), ",")
-	var tags2 []string
-	for _, tag := range tags {
-		tags2 = append(tags2, strings.TrimSpace(tag))
-	}
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
 
+func readBody(scanner *bufio.Scanner) string {
 	scanner.Scan() // ignore a line
-
 	buf := bytes.Buffer{}
 	for scanner.Scan() {
 		fmt.Fprintln(&buf, scanner.Text())
 	}
-	body := strings.TrimSuffix(buf.String(), "\n")
-
-	return Post{
-		Title:       title,
-		Description: description,
-		Tags:        tags2,
-		Body:        body,
-	}, nil
+	return strings.TrimSuffix(buf.String(), "\n")
 }
