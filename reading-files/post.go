3c123e4230ee526b6580d03f0277b1ff99f48b78
n4to4 <n4to4k@gmail.com>
2021-07-13 18:28:30 +0900

    refactor

---
+++ reading-files/post.go
@@ -0,0 +1,24 @@
+package blogposts
+
+import (
+	"bufio"
+	"io"
+)
+
+type Post struct {
+	Title, Description string
+}
+
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile)
+
+	readLine := func() string {
+		scanner.Scan()
+		return scanner.Text()
+	}
+
+	title := readLine()[7:]
+	description := readLine()[13:]
+
+	return Post{Title: title, Description: description}, nil
+}
