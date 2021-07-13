81ab7cdc750fc6e9415a9d1fbaca7d7ec43cd192
n4to4 <n4to4k@gmail.com>
2021-07-13 18:31:39 +0900

    refactor

--- reading-files/post.go
+++ reading-files/post.go
@@ -3,6 +3,7 @@ package blogposts
 import (
 	"bufio"
 	"io"
+	"strings"
 )
 
 const (
@@ -17,13 +18,13 @@ type Post struct {
 func newPost(postFile io.Reader) (Post, error) {
 	scanner := bufio.NewScanner(postFile)
 
-	readLine := func() string {
+	readLine := func(tagName string) string {
 		scanner.Scan()
-		return scanner.Text()
+		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
 
-	title := readLine()[len(titleSeparator):]
-	description := readLine()[len(descriptionSeparator):]
+	title := readLine(titleSeparator)
+	description := readLine(descriptionSeparator)
 
 	return Post{Title: title, Description: description}, nil
 }
