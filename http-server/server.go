4cd12a97404b7195358e1b62de734977a7ff5675
n4to4 <n4to4k@gmail.com>
2021-07-14 18:10:07 +0900

    pass

--- http-server/server.go
+++ http-server/server.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprintf(w, "20")
+		return
+	}
+	if player == "Floyd" {
+		fmt.Fprintf(w, "10")
+		return
+	}
 }
