dcbc52b6f62a09ac08f49269d51b815a6321c94f
n4to4 <n4to4k@gmail.com>
2021-07-14 19:17:14 +0900

    pass

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -55,8 +55,10 @@ func TestStoreWins(t *testing.T) {
 	}
 	server := &PlayerServer{&store}
 
-	t.Run("it returns accepted on POST", func(t *testing.T) {
-		request := newPostWinRequest("Pepper")
+	t.Run("it records wins on POST", func(t *testing.T) {
+		player := "Pepper"
+
+		request := newPostWinRequest(player)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
@@ -65,6 +67,10 @@ func TestStoreWins(t *testing.T) {
 
 		if len(store.winCalls) != 1 {
 			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+		}
+
+		if store.winCalls[0] != player {
+			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
 		}
 	})
 }
