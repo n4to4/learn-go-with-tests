d86a28ff848eab8dedddc56ed7633a516fab3367
n4to4 <n4to4k@gmail.com>
2021-07-16 19:22:52 +0900

    refactor

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -99,11 +99,7 @@ func TestLeague(t *testing.T) {
 		got := getLeagueFromResponse(t, response.Body)
 		assertStatus(t, response.Code, http.StatusOK)
 		assertLeague(t, got, wantedLeague)
-
-		if response.Result().Header.Get("content-type") != "application/json" {
-			t.Errorf("response did not have content-type of application/json, got %q",
-				response.Result().Header)
-		}
+		assertContentType(t, response, jsonContentType)
 	})
 }
 
@@ -153,6 +149,14 @@ func assertStatus(t testing.TB, got, want int) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got status %d want %d", got, want)
+	}
+}
+
+func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want string) {
+	t.Helper()
+	if response.Result().Header.Get("content-type") != want {
+		t.Errorf("response did not have content-type of %s, got %q",
+			want, response.Result().Header)
 	}
 }
 
