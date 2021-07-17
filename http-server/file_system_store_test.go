c307758367eb3ff14d9ff35c7b4d3acf4716a9ab
n4to4 <n4to4k@gmail.com>
2021-07-17 10:07:19 +0900

    pass

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -20,5 +20,22 @@ func TestFileSystemStore(t *testing.T) {
 		}
 
 		assertLeague(t, got, want)
+
+		got = store.GetLeague()
+		assertLeague(t, got, want)
+	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Chris")
+		want := 33
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
 	})
 }
