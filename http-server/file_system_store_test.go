7cce97f08df7cf012e08e82978623cd3ae61cd0b
n4to4 <n4to4k@gmail.com>
2021-07-18 11:37:15 +0900

    pass

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -74,6 +74,14 @@ func TestFileSystemStore(t *testing.T) {
 		want := 1
 		assertScoreEquals(t, got, want)
 	})
+
+	t.Run("works with an empty file", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, "")
+		defer cleanDatabase()
+
+		_, err := NewFileSystemPlayerStore(database)
+		assertNoError(t, err)
+	})
 }
 
 func createTempFile(t testing.TB, initialData string) (*os.File, func()) {
