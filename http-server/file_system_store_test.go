7393da47aab59237b2146a3ae6b4439f4da17603
n4to4 <n4to4k@gmail.com>
2021-07-18 10:53:40 +0900

    refactor

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -70,7 +69,7 @@ func TestFileSystemStore(t *testing.T) {
 	})
 }
 
-func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+func createTempFile(t testing.TB, initialData string) (*os.File, func()) {
 	t.Helper()
 	tmpfile, err := ioutil.TempFile("", "db")
 
