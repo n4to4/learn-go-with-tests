0d1a6b638fe6e637d370906484a9884a9b65b5bf
n4to4 <n4to4k@gmail.com>
2021-07-18 11:41:02 +0900

    refactor

--- http-server/file_system_store.go
+++ http-server/file_system_store.go
@@ -12,18 +12,10 @@ type FileSystemPlayerStore struct {
 }
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, 0)
-
-	info, err := file.Stat()
+	err := initializePlayerDBFile(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v",
-			file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte("[]"))
-		file.Seek(0, 0)
+		return nil, fmt.Errorf("problem initializing player db file, %v", err)
 	}
 
 	league, err := NewLeague(file)
@@ -37,6 +29,24 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
+}
+
+func initializePlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v",
+			file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
