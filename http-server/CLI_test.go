262cf77528eda73c64eb13d8e9dcb886a2504ea3
n4to4 <n4to4k@gmail.com>
2021-07-18 18:27:17 +0900

    pass

--- http-server/CLI_test.go
+++ http-server/CLI_test.go
@@ -6,14 +6,26 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	in := strings.NewReader("Chris wins\n")
-	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore, in}
-	cli.PlayPoker()
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &StubPlayerStore{}
+		cli := &CLI{playerStore, in}
+		cli.PlayPoker()
 
-	if len(playerStore.winCalls) != 1 {
-		t.Fatal("expected a win call but didn't get any")
-	}
+		if len(playerStore.winCalls) != 1 {
+			t.Fatal("expected a win call but didn't get any")
+		}
 
-	assertPlayerWin(t, playerStore, "Chris")
+		assertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &StubPlayerStore{}
+
+		cli := &CLI{playerStore, in}
+		cli.PlayPoker()
+
+		assertPlayerWin(t, playerStore, "Cleo")
+	})
 }
