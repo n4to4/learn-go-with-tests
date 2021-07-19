81efd4f5f649ac7cc4bddc3631150d45043024ee
n4to4 <n4to4k@gmail.com>
2021-07-19 21:51:51 +0900

    pass

--- http-server/cmd/cli/main.go
+++ http-server/cmd/cli/main.go
@@ -19,5 +19,8 @@ func main() {
 
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
-	poker.NewCLI(store, os.Stdin, poker.BlindAlerterFunc(poker.StdOutAlerter)).PlayPoker()
+
+	game := poker.NewGame(poker.BlindAlerterFunc(poker.StdOutAlerter), store)
+	cli := poker.NewCLI(os.Stdin, os.Stdout, game)
+	cli.PlayPoker()
 }
